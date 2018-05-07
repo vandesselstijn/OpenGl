b8eea4eca3243735c5755af38e38dccc3748b22d
vandesselstijn <vandesselstijn@gmail.com>
2018-05-07 07:52:26 +0200

    move + race

--- makefile
+++ makefile
@@ -4,4 +4,4 @@ zeepkist.o : zeepkist.c
 	cc -c zeepkist.c
 
 zeepkist : zeepkist.o
-	cc zeepkist.c -o zeepkist -LGL -lglut -lm
+	cc zeepkist.c -o zeepkist  -lGL -lglut -lGLU -lm -lglut -lm
--- zeepkist.c
+++ zeepkist.c
@@ -6,32 +6,20 @@
 
 #include "./jpeg/InitJPG.h"
 
-GLdouble xlens = 3, ylens = 3, zlens = 10;
+GLint move = 0, race = 0;
+GLdouble zeepkist_x = 0;
+//GLdouble xlens = 3, ylens = 3, zlens = 10;
+GLdouble xlens = 9, ylens = 9, zlens = 30;
+//GLdouble xlens = 6, ylens = 6, zlens = 20;
 GLdouble xref = 1.0, yref = 1.0, zref = 0.0;
 GLdouble xvw = 0.0, yvw = 1.0, zvw = 0.0;
 GLdouble xmin = -2.0, xmax = 4.0, ymin = -2.0, ymax = 3.0;
-GLdouble near = 1.0, far = 100;
+GLdouble near = 1.0, far = 1000;
 GLdouble hoek = 60.0;
 GLfloat ttx = 0.25, tty = 0.25, ttz = 0.25;
 GLint lijnen = 0;
 GLubyte projectie = 'p';
 GLdouble verhouding = 0;
-GLdouble h = 0;
-GLdouble h_deg = 0;
-
-GLdouble xelips [] = {0.00, 4.06, 5.68, 7.01, 8.42, 10.66, 12.26, 13.96, 15.41};
-GLdouble zelips [] = {0.00, 3.06, 5.00, 7.71, 9.42, 10.00, 11.26, 14.46, 16.41};
-GLdouble hoeksnelh [] = {0.0, 0.2, 0.6, 1.0, 1.9, 11.9, 29.5, 84, 164.8};
-GLdouble planhoek[] =  {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-GLdouble straal [] = {1.0, 0.3, 0.50,  0.55, 0.4, 1.05, 1.03, 0.85, 0.825};
-
-GLfloat wit[] = { 1.0, 1.0, 1.0, 1.0 };
-GLfloat zwart[] = { 0.0, 0.0, 0.0, 1.0 };
-GLfloat lichtpunt0 [] = { 0.0, 0.0, 0.0, 1.0 };
-GLfloat r [] = { 1.0, 0.0, 0.0, 1.0 };
-GLfloat g [] = { 0.0, 1.0, 0.0, 1.0 };
-GLfloat b [] = { 0.0, 0.0, 1.0, 1.0 };
-
 
 #define ZESTIEN 16
 #define ZES 6
@@ -70,6 +58,8 @@ void toetsen( unsigned char key, int x, int y)
     case 'u' : xref++;   break;     case 'U' : xref--;   break;
     case 'v' : yref++;   break;     case 'V' : yref--;   break;
     case 'w' : zref++;   break;     case 'W' : zref--;   break;
+    case 'm' : move = 0 ;   break;  case 'M' : move = 1; break;
+    case 'r' : race = 0 ;   break;  case 'R' : race = 1; break;
     case 'j' : xvw = 1.0; yvw = zvw = 0.0;   break;
     case 'k' : yvw = 1.0; xvw = zvw = 0.0;   break;
     case 'l' : zvw = 1.0; yvw = xvw = 0.0;   break;
@@ -116,13 +106,17 @@ void assen()
   glPopMatrix();
 }
 
-#define slices  16
-#define stacks  16
+
+
+#define slices  8
+#define stacks  8
 
 #define tire_diameter 2
 #define tire_height   0.8
-#define tire_width    1.5
-void wiel()
+#define tire_width_b  1.5
+#define tire_width_s  0.3
+
+void wiel(float tire_width)
 {
   GLUquadricObj *tire;
   tire = gluNewQuadric();
@@ -152,21 +146,129 @@ void wiel()
   gluCylinder( tire, tire_diameter-tire_height, tire_diameter-tire_height, tire_width, slices, stacks);
 }
 
+#define wheel_ofset     1
+#define tube_diameter   0.1
+#define frame_width     7.0
+#define frame_lengt     4.0
+#define small_gap       0.5
+#define head_tube       0.4
+#define stack_height    1.5
+
+void onderstel_tube(float tube_lengt)
+{
+
+
+    GLUquadricObj *frame;
+    frame = gluNewQuadric();
+    glColor3f(1.0,0.0,0.0);
+
+    //tube end
+    glPushMatrix();
+    gluDisk(frame, 0, tube_diameter,  slices, stacks);
+
+    glTranslatef(0, 0, tube_lengt);
+    gluDisk(frame, 0, tube_diameter,  slices, stacks);
+    glPopMatrix();
+
+    //tube
+    gluCylinder( frame, tube_diameter, tube_diameter, tube_lengt, slices, stacks);
+}
+
+void onderstel()
+{
+    wiel(tire_width_b);
+
+    //Tube breedte richting
+    onderstel_tube(frame_width);
+
+    //Tube lengte richting
+    glPushMatrix();
+    glRotatef(90.0, 0.0, 1.0, 0.0);
+    glTranslatef(-frame_width/2 , 0, 0);
+    onderstel_tube(frame_lengt);
+    glPopMatrix();
+
+    //2de wiel
+    glPushMatrix();
+    glRotatef(180.0, 0.0, 1.0, 0.0);
+    glTranslatef(0 , 0, -frame_width);
+    wiel(tire_width_b);
+    glPopMatrix();
+
+    //3de (klein) wiel
+    glPushMatrix();
+    glTranslatef(frame_lengt+tire_diameter+wheel_ofset , 0, frame_width/2-tire_width_s/2);
+    wiel(tire_width_s);
+    glPopMatrix();
+
+    //staane buis stuurinrichting
+    glPushMatrix();
+    glTranslatef(frame_lengt , 0, frame_width/2);
+    glRotatef(60.0, 0.0, 0.0, 1.0);
+    glRotatef(90.0, 0.0, 1.0, 0.0);
+    onderstel_tube(stack_height);
+    glPopMatrix();
+
+    //stuurstang
+    glPushMatrix();
+    glTranslatef(frame_lengt , 0, frame_width/2);
+    glRotatef(-30.0, 0.0, 0.0, 1.0);
+    glRotatef(90.0, 0.0, 1.0, 0.0);
+    glTranslatef(0 , stack_height, -tire_diameter);
+    onderstel_tube(tire_diameter);
+    glPopMatrix();
+
+    //vork
+    glPushMatrix();
+    glTranslatef(frame_lengt , 0, frame_width/2);
+    glRotatef(-30.0, 0.0, 0.0, 1.0);
+    glRotatef(90.0, 0.0, 1.0, 0.0);
+    glTranslatef(0 , stack_height, 0);
+    onderstel_tube(head_tube);
+    glPushMatrix();
+    glRotatef(-90.0, 0.0, 1.0, 0.0);
+    glTranslatef(head_tube , 0, -tire_width_s/2-small_gap);
+    onderstel_tube(tire_width_s+2*small_gap);
+    glPopMatrix();
+    glPushMatrix();
+    glTranslatef(-tire_width_s/2-small_gap , 0, head_tube);
+    onderstel_tube(tire_diameter+small_gap);
+    glTranslatef(tire_width_s+2*small_gap, 0, 0);
+    onderstel_tube(tire_diameter+small_gap);
+    glPopMatrix();
+
+    glPopMatrix();
+}
+
 //Drawing funciton
 void zeepkist(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  gluLookAt(xlens,ylens,zlens, xref,yref,zref, xvw, yvw, zvw);
-  
-  //assen tekenen
-  assen();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(xlens,ylens,zlens, xref,yref,zref, xvw, yvw, zvw);
 
-  wiel();
+    //assen tekenen
+    //assen();
 
-  
-  glutSwapBuffers();
+    glPushMatrix();
+    glTranslatef(zeepkist_x , 0, 0);
+    onderstel();
+    glPopMatrix();
+
+    if(race)
+    {
+        glPushMatrix();
+        glTranslatef(zeepkist_x , 0, frame_width+tire_width_b);
+        onderstel();
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(zeepkist_x , 0, -frame_width-tire_width_b);
+        onderstel();
+        glPopMatrix();
+    }
+
+    glutSwapBuffers();
 }
 
 void herschaal(GLint n_w, GLint n_h) 
@@ -190,51 +292,46 @@ void herschaal(GLint n_w, GLint n_h)
     case 'F': grens = near*tan(M_PI*(hoek/2.0)/180.0);
         glFrustum(-grens, grens,  -grens, grens, near, far);
         break;
-  }
+    }
   
-  verhouding = xmax/ymax;
+    verhouding = xmax/ymax;
   
-  if(n_w <= n_h * verhouding)
-  {
-    glViewport(0,0, n_w, n_w/verhouding);
-  }
-  else
-  {
-    glViewport(0,0, verhouding*n_h, n_h);
-  }
-  
-  //glViewport(0, 0, n_w, n_h);
+    if(n_w <= n_h * verhouding)
+    {
+        glViewport(0,0, n_w, n_w/verhouding);
+    }
+    else
+    {
+        glViewport(0,0, verhouding*n_h, n_h);
+    }
+    //glViewport(0, 0, n_w, n_h);
 }
 
 void animate()
 {
-  h += 1;
-  
-  for(int i = 1; i<8; i++)
-  {
-    planhoek[i]= h*(1/hoeksnelh[i])*(M_PI/180);
-    
-  }
-    
-  /*h_deg = h*(M_PI/180);*/
-  glutPostRedisplay();
+    if(move)
+    {
+        zeepkist_x += 0.1;
+        if(zeepkist_x >= 100)
+            zeepkist_x = 0;
+        glutPostRedisplay();
+    }
 }
 
 //Main program
 int main(int argc, char **argv)
 {
-  glutInit(&argc, argv);
-  //Simple buffer
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
-  glutInitWindowPosition(50,25);
-  glutInitWindowSize(500,250);
-  glutCreateWindow("Zeepkist");
-    myinit();
-  glutKeyboardFunc(toetsen);
-  glutReshapeFunc(herschaal);
-  //Call to the drawing function
-  glutDisplayFunc(zeepkist);
-  glutIdleFunc(animate);
-  glutMainLoop();
-  return 0;
+      glutInit(&argc, argv);
+      //Simple buffer
+      glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+      glutInitWindowPosition(0,0);
+      glutInitWindowSize(1366,768);
+      glutCreateWindow("Zeepkist");myinit();
+      glutKeyboardFunc(toetsen);
+      glutReshapeFunc(herschaal);
+      //Call to the drawing function
+      glutDisplayFunc(zeepkist);
+      glutIdleFunc(animate);
+      glutMainLoop();
+      return 0;
 }
